6c7588a1e57f59a91f708934f087bca6ea05e6f7
Henrique M. Dimer <hdimer@tutamail.com>
2025-04-24 14:39:13 -0300

    chore: add warning comments in file header

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
+/*
+ *	---> CODIGO DESENVOLVIDO E TESTADO APENAS NO LINUX (6.12.24-0-lts, x86_64, musl)! <---
+ *	Se ocorrer algum bug, por favor, tente rodar no Linux.
+ *
+ * 	Para compilar o codigo usando MAKE, apenas rode:
+ * 		- make
+ * 	Usando gcc:
+ * 		- gcc ./main.c -O3 -o jogo
+ */
+
 #include <stdio.h>
 
 int main()

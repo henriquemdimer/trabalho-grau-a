c7b8329b3543149540d4ebeedfc20c47dd0902f6
Henrique M. Dimer <hdimer@tutamail.com>
2025-04-26 17:24:42 -0300

    chore: prevent clang-format from breaking header comments

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
 /*
- *	---> CODIGO DESENVOLVIDO E TESTADO APENAS NO LINUX (6.12.24-0-lts, x86_64, musl)! <---
- *	Se ocorrer algum bug, por favor, tente rodar no Linux.
+ *	---> CODIGO DESENVOLVIDO E TESTADO APENAS NO LINUX (6.12.24-0-lts, x86_64, musl) <---
+ *	Se acontecer algum bug no Windows, por favor, tente rodar no Linux.
  *
  * 	Para compilar o codigo usando MAKE, apenas rode:
  * 		- make
  * 	Usando gcc:
  * 		- gcc ./main.c -O3 -o jogo
+ *
+ * 	Github: https://github.com/henriquemdimer/trabalho-grau-a
+ * 	clang-format on
  */
 
 #include <stdio.h>
 
 int main()
 {
-	printf("Hello World!\n");
+        printf("Hello World!\n");
         return 0;
 }
